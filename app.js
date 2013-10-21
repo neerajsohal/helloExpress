b4647cca07b086bf48f8c466a687ce533718eb1a
Neeraj Kumar <codemaster.snake@gmail.com>
2013-10-21 13:15:40 +0530

    added bootstrap. created hello route

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get("/hello/:who", function(req, res) {
+  res.end("Hello, " + req.params.who + "! How are you Today!");
+  // Fun fact: this has security issues
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
